049932eea09a12964a16a8d9f45a0a82ef8c5f0f
Holt Skinner <holtskinner@google.com>
2022-06-09 14:35:28 -0500

    Init Custom Training Data

---
+++ convert-training-data/main.py
@@ -0,0 +1,13 @@
+"""
+Converts Training Data to Document AI Format
+"""
+from google.cloud import bigquery, storage, documentai_v1 as documentai
+
+
+def read_from_bq():
+    bq_client = bigquery.Client()
+    return
+
+
+def convert_classification_data():
+    return
